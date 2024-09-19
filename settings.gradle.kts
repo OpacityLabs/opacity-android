eb07578e4bfebf636d607bec111dc11efad9b226
Oscar Franco <ospfranco@gmail.com>
2024-09-19 15:18:55 -0400

    Prepare for JitPack publication

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,14 +12,17 @@ pluginManagement {
         maven("https://maven.mozilla.org/maven2/")
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
         maven("https://maven.mozilla.org/maven2/")
+        maven("https://jitpack.io")
     }
 }
+
 
 rootProject.name = "OpacityCoreExample"
 include(":app")
