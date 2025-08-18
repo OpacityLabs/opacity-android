cafb3c849988075299589f09776c46f818bd6d4d
Oscar Franco <ospfranco@gmail.com>
2025-08-18 16:40:39 +0200

    update

--- OpacityCore/src/main/cpp/OpacityCore.cpp
+++ OpacityCore/src/main/cpp/OpacityCore.cpp
@@ -20,17 +20,17 @@ void DeferThreadDetach(JNIEnv *env) {
   // This is only done once, across all threads, and the value
   // associated with the key for any given thread will initially
   // be NULL.
-  static auto run_once = [] {
-    const auto err = pthread_key_create(&thread_key, [](void *ts_env) {
-      if (ts_env) {
-        java_vm->DetachCurrentThread();
-      }
-    });
-    if (err) {
-      // Failed to create TSD key. Throw an exception if you want to.
-    }
-    return 0;
-  }();
+  // static auto run_once = [] {
+  //   const auto err = pthread_key_create(&thread_key, [](void *ts_env) {
+  //     if (ts_env) {
+  //       java_vm->DetachCurrentThread();
+  //     }
+  //   });
+  //   if (err) {
+  //     // Failed to create TSD key. Throw an exception if you want to.
+  //   }
+  //   return 0;
+  // }();
 
   // For the callback to actually be executed when a thread exits
   // we need to associate a non-NULL value with the key on that thread.
