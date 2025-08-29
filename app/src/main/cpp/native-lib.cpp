24ca6338f176e54d0d594b3ab46a34faca4301cf
0xwildhare <sschell1979@gmail.com>
2025-08-29 07:52:51 -0700

    Uncomment setEnv() call in MainActivity and comment out environment variable settings in native-lib.cpp

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,6 +4,6 @@
 extern "C" JNIEXPORT void JNICALL
 Java_com_opacitylabs_opacitycoreexample_MainActivity_setEnv(JNIEnv *env,
                                                             jclass clazz) {
-    setenv("OPACITY_ALLOW_SNIFFING", "1", 1);
-//    setenv("OPACITY_BACKEND_URL", "https://quack.com", 1);
+    // setenv("OPACITY_ALLOW_SNIFFING", "1", 1);
+    // setenv("OPACITY_BACKEND_URL", "https://quack.com", 1);
 }
