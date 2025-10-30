0c8877a7804367fa08148b1c317c1003b72aa9ed
Lukas <lukasogunfeitimi@gmail.com>
2025-10-30 06:37:35 +0000

    change payload structure

--- OpacityCore/src/main/assets/interceptExtension/background.js
+++ OpacityCore/src/main/assets/interceptExtension/background.js
@@ -3,7 +3,8 @@ browser.runtime.onMessage.addListener(function (message, sender, _) {
     try {
       browser.runtime.sendNativeMessage("gecko", {
         event: "intercepted_request",
-        data: message.data,
+        request_type: message.data.request_type,
+        data: message.data.data,
       });
     } catch (err) {
       // Silently handle errors to prevent crashes
